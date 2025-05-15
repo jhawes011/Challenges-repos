fd7a8b3fc304c1549636850ac1685493749dc1ba
Joe Hawes <jhawes011@gmail.com>
2025-05-14 21:30:51 -0400

    Initi Commit Public challenges projects
    This repo will include any off to the side coding challenges that i complete, using coding challenge websites such as LettCode or HackerRank

---
+++ leetcode-c#-two-sum/leetcode-c#-two-sum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_c__two_sum
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int[] nums = { 2, 7, 11, 15 };
+			int target = 9;
+			int[] result = new Program().TwoSum(nums, target);
+			Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+		}
+
+		public int[] TwoSum(int[] nums, int target)
+		{
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[j] == target - nums[i])
+					{
+						return new int[] { i, j };
+					}
+				}
+			}
+			return new int[0] { };
+		}
+	}
+}
