9d44651068008b5160c8f54fccfe59de362dfef2
Joe Hawes <jhawes011@gmail.com>
2025-05-22 20:46:44 -0400

    leetcode-longest-common-prefix

---
+++ leetcode-c#-longest-common-prefix/leetcode-c#-longest-common-prefix/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_c__longest_common_prefix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{ 
+			string LongestCommonPrefix(string[] strs)
+		{
+			if (strs == null || strs.Length == 0) return "";
+			string prefix = strs[0];
+			for (int i = 1; i < strs.Length; i++)
+			{
+				while (strs[i].IndexOf(prefix) != 0)
+				{
+					prefix = prefix.Substring(0, prefix.Length - 1);
+
+				}
+
+			}
+			return prefix;
+		} }
+
+
+
+			
+	}
+	
+    }
+
